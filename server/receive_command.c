ede6036a8e804dd0282c352e674353e5e7933fe9
Mahe-Evan <evan.mahe@epitech.eu>
2025-04-17 17:18:54 +0200

    add: 🚧 | add commands receive print

--- server/receive_command.c
+++ server/receive_command.c
@@ -5,26 +5,31 @@
 ** check and call the commands
 */
 
+#include "jetpack.h"
+#include "server.h"
 #include <stdbool.h>
+#include <stdio.h>
 #include <string.h>
 #include <strings.h>
 #include <unistd.h>
-#include <stdio.h>
-#include "server.h"
-#include "jetpack.h"
 
-void receive_command(server_t *restrict server, player_t *restrict player,
-    int client_index, bool ready)
+void receive_command(server_t *restrict server,
+    player_t *restrict player, int client_index, bool ready)
 {
     char is_flying = '0';
 
-    if (strlen(player->command) == 5 &&
-        sscanf(player->command, "FLY %c", &is_flying) == 1) {
+    if (sscanf(player->command, "FLY %c", &is_flying) == 1) {
         player->is_flying = is_flying - '0';
         printf("FLY %c FROM %d\n", is_flying, client_index);
+        write(server->fds[client_index + 2].fd, "OK\r\n", 4);
     } else if (!ready && strcmp(player->command, "READY") == 0) {
         server->game.ready_player[client_index] = true;
         printf("READY FROM %d\n", client_index + 1);
+        write(server->fds[client_index + 2].fd, "OK\r\n", 4);
+    } else {
+        write(server->fds[client_index + 2].fd,
+            "ERROR Unknown command\r\n", 22);
     }
+    printf("Command received: %s\n", player->command);
     return;
 }
