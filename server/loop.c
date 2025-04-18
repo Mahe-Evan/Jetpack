846049b97933473ff35f18e186c093566773cf60
Renaud Manet <renaud.manet@epitech.eu>
2025-04-18 13:48:48 +0200

    style: :art: put the loop logic inside a separate function

--- server/loop.c
+++ server/loop.c
@@ -5,17 +5,17 @@
 ** main loop
 */
 
+#include "jetpack.h"
+#include "server.h"
 #include <errno.h>
 #include <netinet/in.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/poll.h>
-#include <stdlib.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include "server.h"
-#include "jetpack.h"
 
 static void set_player(int player_fd, char *map[static MAP_LEN])
 {
@@ -31,8 +31,9 @@ static void set_player(int player_fd, char *map[static MAP_LEN])
     return;
 }
 
-static bool handle_new_connection(pollfd_t
-    fds[static restrict MAX_CLIENTS + 2], char *map[static MAP_LEN])
+static bool handle_new_connection(
+    pollfd_t fds[static restrict MAX_CLIENTS + 2],
+    char *map[static MAP_LEN])
 {
     struct sockaddr_in client_addr;
     socklen_t addr_len = sizeof(client_addr);
@@ -41,18 +42,21 @@ static bool handle_new_connection(pollfd_t
     if (fds[MAX_CLIENTS + 1].fd != -1 || !(fds[1].revents & POLLIN)) {
         return true;
     }
-    client_fd = accept(fds[1].fd, (struct sockaddr *)&client_addr, &addr_len);
+    client_fd =
+        accept(fds[1].fd, (struct sockaddr *)&client_addr, &addr_len);
     if (client_fd == -1) {
         perror("accept");
         return false;
     }
-    for (; fds->fd != -1; fds++);
+    for (; fds->fd != -1; fds++)
+        ;
     fds->fd = client_fd;
     set_player(client_fd, map);
     return true;
 }
 
-static bool check_player_ready(bool ready_player[static restrict MAX_CLIENTS],
+static bool check_player_ready(
+    bool ready_player[static restrict MAX_CLIENTS],
     int nb_player_required)
 {
     int nb_ready = 0;
@@ -71,9 +75,11 @@ static bool check_player_ready(bool ready_player[static restrict MAX_CLIENTS],
     return false;
 }
 
-static void send_start(bool *restrict is_enough_player_ready,
+static void send_start(
+    bool *restrict is_enough_player_ready,
     bool ready_player[static MAX_CLIENTS],
-    pollfd_t fds[static restrict MAX_CLIENTS + 2])
+    pollfd_t fds[static restrict MAX_CLIENTS + 2],
+    bool is_alive[static MAX_CLIENTS])
 {
     *is_enough_player_ready = check_player_ready(ready_player, 2);
     if (!(*is_enough_player_ready)) {
@@ -82,9 +88,29 @@ static void send_start(bool *restrict is_enough_player_ready,
     for (int i = 2; fds[i].fd != -1 && i < MAX_CLIENTS + 2; i++) {
         if (ready_player[i - 2]) {
             write(fds[i].fd, "START\r\n", 7);
+            is_alive[i - 2] = true;
         }
     }
     return;
+}
+
+static void handle_server_loop_iteration(server_t *restrict server,
+    bool *is_enough_player_ready, bool *restrict is_running)
+{
+    int poll_res = poll(server->fds, MAX_CLIENTS + 2, 1000);
+
+    if (poll_res == -1) {
+        perror("poll");
+        return false;
+    }
+    if (!*is_enough_player_ready) {
+        *is_running = handle_new_connection(server->fds, server->game.map);
+        send_start(is_enough_player_ready, server->game.ready_player,
+                  server->fds, server->is_alive);
+    }
+    if (!handle_existing_clients(server, *is_enough_player_ready))
+        return false;
+    return is_running ? handle_signal(&server->fds[0]) : is_running;
 }
 
 int loop(server_t *restrict server)
@@ -94,18 +120,8 @@ int loop(server_t *restrict server)
     bool is_enough_player_ready = false;
 
     while (is_running) {
-        poll_res = poll(server->fds, MAX_CLIENTS + 2, 1000);
-        if (poll_res == -1) {
-            perror("poll");
-            break;
-        }
-        if (!is_enough_player_ready) {
-            is_running = handle_new_connection(server->fds, server->game.map);
-            send_start(&is_enough_player_ready,
-                server->game.ready_player, server->fds);
-        }
-        handle_existing_clients(server, is_enough_player_ready);
-        is_running = is_running ? handle_signal(&server->fds[0]) : is_running;
+        handle_server_loop_iteration(server, &is_enough_player_ready,
+            &is_running);
     }
     return errno ? EXIT_FAILURE : EXIT_SUCCESS;
 }
